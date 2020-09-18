87472823420515e6d641f3300e6d1d2bc654088f
molk95 <molk.saouabi@gmail.com>
2020-09-18 19:38:57 +0200

    products

--- src/compnents/js/Home.js
+++ src/compnents/js/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 import Product from "../js/Product";
 import AmazonPrime from "../images/AmazonPrime.jpg";
 import CODINGbook from "../images/CODINGbook.jpg";
+import Asp from '../images/asp_.jpg'
+import cap from '../images/bcap.jpg'
+import cat from '../images/cat.jpg'
+import unicorn from '../images/unicorn.jpg'
+
 import "../css/Home.css";
 function Home() {
   return (
@@ -16,8 +21,38 @@ function Home() {
           rating={5}
           image={CODINGbook}
         />
+        <Product
+          id="2"
+          title="Rowenta RY8544WH Clean & Steam Multi, ​nettoyeur Vapeur 2 en 1, Nettoie et Aspire en Même Temps à la Vapeur Tous les Sols et Toutes les Sufaces Performant"
+          price="292.99"
+          rating={5}
+          image={Asp}
+        />
+       
       </div>
-      <div className="home__row"></div>
+      <div className="home__row">
+      <Product
+          id="3"
+          title="Tasses en céramique pour lait, café, thé–tasse pour petit déjeuner avec animal 3D, Cadeau idéal pour boisson du matin, ou pour les mariages, anniversaires, Céramique, 8oz "
+          price="12.99"
+          rating={3}
+          image={cap}
+        />
+      <Product
+          id="4"
+          title="Tasses en céramique pour lait, café, thé–tasse pour petit déjeuner avec animal 3D, Cadeau idéal pour boisson du matin, ou pour les mariages, anniversaires, Céramique, 8oz "
+          price="14.20"
+          rating={3}
+          image={unicorn}
+        />
+      <Product
+          id="5"
+          title="Tasses en céramique pour lait, café, thé–tasse pour petit déjeuner avec animal 3D, Cadeau idéal pour boisson du matin, ou pour les mariages, anniversaires, Céramique, 8oz "
+          price="16.95"
+          rating={3}
+          image={cat}
+        />
+      </div>
       <div className="home__row"></div>
     </div>
   );
