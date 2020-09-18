1e76341c524875134fc6f84b5be9dbf5c9f85b02
molk95 <molk.saouabi@gmail.com>
2020-09-18 16:44:34 +0200

    router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import Header from '../compnents/js/Header';
-import Home from '../compnents/js/Home.js';
-import './App.css';
+import React from "react";
+import Header from "../compnents/js/Header";
+import Home from "../compnents/js/Home.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    
     <div className="app">
-      <Header />
-      <Home />
+      <Router>
+        <Switch>
+          <Header />
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/login">LOGIN</Route>
+          <Route path="/checkout">
+            <h1>Passer la commande</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
